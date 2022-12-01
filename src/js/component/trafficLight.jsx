cb387204bd6ed1aacbd3257bfb9cf1416bdf4ef8
strps <csrstrps@gmail.com>
2022-12-01 21:20:25 +0000

    Adding the cycling through all the ligths to the corresponding button

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -1,29 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function TrafficLigth(props) {
 
     const [lightOn, setLightOn] = useState(-1)
-    const [purple, setPurple] = useState(false)
-    const [cycle, setCycle] = useState(false)
+    const [lightsQuantity, setPurple] = useState(3)
+    const [cycle, setCycle] = useState(true)
+
+    useEffect(() => {
+        const id = setInterval(() => {
+            if (cycle) {
+                setLightOn(a => ((a < lightsQuantity-1) ? a + 1 : 0))
+            }
+        }, 1000)
+        return () => {
+            clearInterval(id)
+        }
+    }, [cycle, lightsQuantity])
 
     let purpleButton = <button className={"light purple" + ((lightOn == 3) ? "-on" : "")} onClick={() => setLightOn(3)}></button>
-
-    let cycleButtonHadler = () => {
-
-
-    }
 
     return (
         <div className="traffic-light">
             <button className={"light red" + ((lightOn == 0) ? "-on" : "")} onClick={() => setLightOn(0)}></button>
             <button className={"light yellow" + ((lightOn == 1) ? "-on" : "")} onClick={() => setLightOn(1)}></button>
             <button className={"light green" + ((lightOn == 2) ? "-on" : "")} onClick={() => setLightOn(2)}></button>
-            {purple ? purpleButton : ""}
-            <button className={'ctrl-button' + (cycle ? "-on" : "")} onClick={cycleButtonHadler}><i className="bi bi-arrow-repeat"></i></button>
-            <button className={'ctrl-button' + (purple ? "-on" : "")} onClick={() => setPurple(purple ? false : true)}><i className="bi bi-plus-lg"></i></button>
+            {lightsQuantity == 4? purpleButton : ""}
+            <button className={'ctrl-button' + (cycle ? "-on" : "")} onClick={() => setCycle(cycle ? false : true)}><i className="bi bi-arrow-repeat"></i></button>
+            <button className={'ctrl-button' + (lightsQuantity == 4 ? "-on" : "")} onClick={() => setPurple(lightsQuantity == 4? 3 : 4)}><i className="bi bi-plus-lg"></i></button>
         </div>)
 
 }
 
-
 export default TrafficLigth
+
+
