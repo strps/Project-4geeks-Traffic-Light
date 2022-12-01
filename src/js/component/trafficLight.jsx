6104b92e7cc493e4bcf49e6cf8725c12965f1307
strps <csrstrps@gmail.com>
2022-12-01 07:50:36 +0000

    Completing the design, adding the bonuthe add purple button with its functionality, and adding the cycle button with no functionality implemented

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from 'react';
 
-class TrafficLigth extends React.Component {
-    render(){
-        return(<div className="traffic-light">
-            <button className="light red"></button>
-            <button className="light yellow"></button>
-            <button className="light green"></button>
-        </div>)
+function TrafficLigth(props) {
+
+    const [lightOn, setLightOn] = useState(-1)
+    const [purple, setPurple] = useState(false)
+    const [cycle, setCycle] = useState(false)
+
+    let purpleButton = <button className={"light purple" + ((lightOn == 3) ? "-on" : "")} onClick={() => setLightOn(3)}></button>
+
+    let cycleButtonHadler = () => {
+
+
     }
+
+    return (
+        <div className="traffic-light">
+            <button className={"light red" + ((lightOn == 0) ? "-on" : "")} onClick={() => setLightOn(0)}></button>
+            <button className={"light yellow" + ((lightOn == 1) ? "-on" : "")} onClick={() => setLightOn(1)}></button>
+            <button className={"light green" + ((lightOn == 2) ? "-on" : "")} onClick={() => setLightOn(2)}></button>
+            {purple ? purpleButton : ""}
+            <button className={'ctrl-button' + (cycle ? "-on" : "")} onClick={cycleButtonHadler}><i className="bi bi-arrow-repeat"></i></button>
+            <button className={'ctrl-button' + (purple ? "-on" : "")} onClick={() => setPurple(purple ? false : true)}><i className="bi bi-plus-lg"></i></button>
+        </div>)
+
 }
+
 
 export default TrafficLigth
