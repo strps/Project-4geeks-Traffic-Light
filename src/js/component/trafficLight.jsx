24467ba8459e3903435bbd2e03a7cd2f7c9ceeba
strps <csrstrps@gmail.com>
2022-12-15 01:51:57 +0000

    Mapping each color from an array

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -17,14 +17,11 @@ function TrafficLigth(props) {
         }
     }, [cycle, lightsQuantity])
 
-    let purpleButton = <button className={"light purple" + ((lightOn == 3) ? "-on" : "")} onClick={() => setLightOn(3)}></button>
+    const colors = ["red", "yellow", "green", "purple"]
 
     return (
         <div className="traffic-light">
-            <button className={"light red" + ((lightOn == 0) ? "-on" : "")} onClick={() => setLightOn(0)}></button>
-            <button className={"light yellow" + ((lightOn == 1) ? "-on" : "")} onClick={() => setLightOn(1)}></button>
-            <button className={"light green" + ((lightOn == 2) ? "-on" : "")} onClick={() => setLightOn(2)}></button>
-            {lightsQuantity == 4? purpleButton : ""}
+            {colors.slice(0, lightsQuantity).map((e, i)=><button className={"light "+ e + ((lightOn == i) ? "-on" : "")} onClick={() => setLightOn(3)}></button>)}
             <button className={'ctrl-button' + (cycle ? "-on" : "")} onClick={() => setCycle(cycle ? false : true)}><i className="bi bi-arrow-repeat"></i></button>
             <button className={'ctrl-button' + (lightsQuantity == 4 ? "-on" : "")} onClick={() => setPurple(lightsQuantity == 4? 3 : 4)}><i className="bi bi-plus-lg"></i></button>
         </div>)
@@ -32,5 +29,3 @@ function TrafficLigth(props) {
 }
 
 export default TrafficLigth
-
-
